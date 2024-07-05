95609c18c6b4f1127465898ead70e799e1d2277e
mariaicec <mgcv675@gmail.com>
2024-07-04 18:04:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// Si deseas agregar interactividad o funcionalidad, puedes hacerlo aquí.
+// Por ejemplo, podrías añadir efectos al hacer clic en los artículos o cargar dinámicamente contenido adicional.
